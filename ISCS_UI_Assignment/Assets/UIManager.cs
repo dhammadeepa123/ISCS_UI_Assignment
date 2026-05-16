a9ca0d90fa89fd81b70f5d5f530a701a75d782a6
dhammadeepa123 <204305051+dhammadeepa123@users.noreply.github.com>
2026-05-16 22:53:08 +0530

    Complete UI  navigate withc#

---
+++ ISCS_UI_Assignment/Assets/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Panels 4")]
+    public GameObject homePanel;
+    public GameObject shopPanel;
+    public GameObject inventoryPanel;
+    public GameObject settingsPanel;
+
+    void Start()
+    {
+   
+        OpenHome();
+    }
+
+    public void OpenHome()
+    {
+        homePanel.SetActive(true);
+        shopPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenShop()
+    {
+        homePanel.SetActive(false);
+        shopPanel.SetActive(true);
+        inventoryPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenInventory()
+    {
+        homePanel.SetActive(false);
+        shopPanel.SetActive(false);
+        inventoryPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        homePanel.SetActive(false);
+        shopPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+}
